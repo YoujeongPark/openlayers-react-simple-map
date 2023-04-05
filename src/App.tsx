73a2fd09c4b16083aff9af6ee9f86d190c1176e1
Youjeong Park <pyj0695@gmail.com>
2023-04-05 22:04:46 +0900

    feat : delete unnecessary code and add app and index

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import './App.css';
+import MapComponent from './MapComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Test
-      </header>
+    <div>
+        <MapComponent />
     </div>
   );
 }
