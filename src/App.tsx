01b80845784251b8f87cf5b8eecad740a280fc41
Youjeong Park <pyj0695@gmail.com>
2023-04-05 20:23:42 +0900

    feat : typescript init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Test
+      </header>
+    </div>
+  );
+}
+
+export default App;
